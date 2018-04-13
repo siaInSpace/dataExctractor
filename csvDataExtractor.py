5c6430ff71c63de8c8e708fb9e6d7b733e29daf1
siaa20 <sia20@aber.ac.uk>
2018-04-13 16:26:28 +0200

    added readLine function to convert a line to a array in a predefined index

--- csvDataExtractor.py
+++ csvDataExtractor.py
@@ -2,12 +2,30 @@ import matplotlib.pyplot as plt
 f = open("Launch_P-1B.csv", "r")
 INDEX = ["startTime", "endTime", "acc_x", "acc_y", "acc_z", "gyr_x",
          "gyr_y", "gyr_z", "accGyr_temp", "bmp_temp", "pressure", "height"]
+# currIndex =  ,ax/ms2,ay/ms2,az/ms2,ba/m,bp/pa,bt/degc,gps,gx/dsec,gy/dsec,gz/dsec,it/degc,t1,t2
+# currIndex =
+f.readline()
 
 
-def timeConverter(data):  # 2018-03-22 03:22:11.140776
+def timeConverter(data):
+    """convert from date format to seconds"""
     data = data.split(" ")
     data.remove(data[0])
     data = data[0].split(":")
     data[2] = float(data[2]) + float(data[0])*3600
     data[2] = float(data[2]) + float(data[1])*60
     return data[2]
+
+
+def readline(line):
+    """convert from a line to a array in a predefined index"""
+    tempArray = []
+    line = line.split(",")
+    line = line.remove(line[0])
+    line = line.remove(line[6])
+    ind = [0, 1, 2, 6, 7, 8, 9, 5, 4, 3]
+    tempArray.append(timeConverter(line[10]))
+    tempArray.append(timeConverter(line[11]))
+    for i in ind:
+        tempArray.append(line[i])
+    return tempArray
