512e6247d7173dd93b657e1e6e2c1324f6f6a3c2
siaa20 <sia20@aber.ac.uk>
2018-04-13 19:04:08 +0200

    added docStrings

--- csvDataExtractor.py
+++ csvDataExtractor.py
@@ -1,3 +1,11 @@
+"""
+csvDataExtractor.py
+Creator: Sindre Aalhus
+Version: 1.4
+Program to retrive data from a csv file, remodell the structure of the 
+    file and plot the result
+"""
+
 import matplotlib.pyplot as plt
 f = open("Launch_P-1B.csv", "r")
 finalIndex = ["startTime", "endTime", "acc_x", "acc_y", "acc_z", "gyr_x",
@@ -6,7 +14,11 @@ f.readline()
 
 
 def timeConverter(data):
-    """convert from date format to seconds"""
+    """Convert from date format to seconds
+
+    Arguments:
+        data {list} -- the list of data to convert time data to correct format
+    """
     data = data.split(" ")
     data.remove(data[0])
     data = data[0].split(":")
@@ -16,7 +28,11 @@ def timeConverter(data):
 
 
 def readline(line):
-    """convert from a line to a array in a predefined index"""
+    """Convert from a line to a array in a predefined index
+
+    Arguments:
+        line {string} -- the line to convert
+    """
     tempArray = []
     line = line.split(",")
     line.remove(line[0])
@@ -31,7 +47,11 @@ def readline(line):
 
 
 def removeStartTime(sortedData):
-    """removes start time from time values"""
+    """Removes start time from time values
+
+    Arguments:
+        sortedData {list} -- the data sorted with lowest time first
+    """
     t0 = sortedData[0][0]
     for i in range(len(sortedData)):
         sortedData[i][0] = sortedData[i][0] - t0
@@ -40,7 +60,12 @@ def removeStartTime(sortedData):
 
 
 def toSingleArray(sortedTimelessData, index):
-    """convert a index of data to a single array"""
+    """convert a index of data to a single array
+
+    Arguments:
+        sortedTimelessData {list} -- a list sorted and removed lowest time from all values
+        index {int} -- the index of the list to convert
+    """
     tempArray = []
     for i in range(len(sortedTimelessData)):
         tempArray.append(sortedTimelessData[i][index])
@@ -48,7 +73,13 @@ def toSingleArray(sortedTimelessData, index):
 
 
 def afterAndBeforeArray(data, t_start, t_end):
-    """remove data before t_start and after t_end"""
+    """remove data before t_start and after t_end
+
+    Arguments:
+        data {list} -- the list to remove some data from
+        t_start {int} -- the time value to remove all data before
+        t_end {int} -- the time value to remove all data after
+    """
     tempArray = []
     for i in range(len(data)):
         if data[i][0] >= t_start and data[i][0] <= t_end:
@@ -57,12 +88,20 @@ def afterAndBeforeArray(data, t_start, t_end):
 
 
 def plot(data, index):
+    """Plot data with x as time axis
+
+    Arguments:
+        data {list} -- the list of data to plot
+        index {int} -- the index of the list to plot
+    """
     plt.figure(finalIndex[index])
     # plt.scatter(toSingleArray(data, 0), toSingleArray(data, index))
     plt.plot(toSingleArray(data, 0), toSingleArray(data, index))
 
 
-def test():
+def main():
+    """Main function
+    """
     data = []
     line = f.readline()
     while line != "":
@@ -73,7 +112,7 @@ def test():
     data = afterAndBeforeArray(data, 300, 800)
     for i in range(2, len(data[0])):
         plot(data, i)
-    plt.show()  # looked at data, seems luanch at t = 350++ a bit and crash at 700--
+    plt.show()  # looked at data, seems luanch at t = 300++ a bit and crash at 800--
 
 
-test()
+main()
